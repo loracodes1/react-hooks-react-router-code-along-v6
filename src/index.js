9116739df11448292e61ae40e848beda33a4c237
florence <loracodes3@gmail.com>
2024-11-13 19:58:52 +0300

    importing the newly added component to the js index file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import routes from "./routes.js";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Login from "./pages/Login";
 
-const router = createBrowserRouter(routes);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  }, 
+  {
+    path: "/about",
+    element: <About />
+  },
+  {
+    path: "/login",
+    element: <Login />
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<RouterProvider router={router} />);
